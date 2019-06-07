3e9cdee84bbebe07b831d744dfe1d3b37ef7c667
Ivan Sendin <ivansendin@yahoo.com>
2019-06-07 09:07:49 -0300

    Initial commit

---
+++ Base58HashFamily.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import random as R
+import math
+
+
+W_SIZE=32
+
+B58Code = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+    
+class HashBase58():
+    def __init__(self,NUM_HASH,HASH_SIZE):
+        #6bits per digit
+        ES = math.ceil(math.log(HASH_SIZE,2)/6)
+        self.look_up = [ [R.randint(1,W_SIZE-2) for _ in range(ES)] for _ in range(NUM_HASH)]
+        self.HASH_SIZE = HASH_SIZE
+
+    def hash(self,w):
+        for l in self.look_up:
+            h=0
+            for i in range(len(l)):
+                h|= B58Code.index(w[l[i]])<<(i*6)
+            yield h%self.HASH_SIZE
+    
